c2ec56bb8b9e127a19d8d89beda0362b2a7244d5
sambangiadharsh <sambangiadharsh1729@gmail.com>
2025-03-19 18:29:13 +0530

    chat2

--- src/utils/sockets.js
+++ src/utils/sockets.js
@@ -6,7 +6,7 @@ export const CreateSocketConnection=()=>{
         return io(BASE_URL);
     }
     else{
-        return io("/",{path:"/api/socket.io"})
+        return io("/",{path:"/api/socket.io/"})
     }
     
 }
