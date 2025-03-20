b85248610edd1d73d0fcee590506451f022c6331
sambangiadharsh <sambangiadharsh1729@gmail.com>
2025-03-20 21:53:39 +0530

    fgh

--- src/utils/sockets.js
+++ src/utils/sockets.js
@@ -3,14 +3,14 @@ import { io } from "socket.io-client";
 const DEV_BASE_URL = "http://localhost:7777";
 const PROD_BASE_URL = "https://tindev.duckdns.org";
 
-const SOCKET_PATH = "/socket.io/";
+// Use "/socket.io/" in development and "/api/socket.io/" in production
+const SOCKET_PATH = location.hostname === "localhost" ? "/socket.io/" : "/api/socket.io/";
 
 export const CreateSocketConnection = () => {
   const isLocalhost = location.hostname === "localhost";
-
   return io(isLocalhost ? DEV_BASE_URL : PROD_BASE_URL, {
     path: SOCKET_PATH,
-    transports: ["polling", "websocket"], 
+    transports: ["polling", "websocket"],
     withCredentials: true,
   });
 };
